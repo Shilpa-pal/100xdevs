0ad5665727e0a809a374ab63a54f6e5d09170702
Shilpa pal <shilpapal1721@gmail.com>
2025-12-15 23:15:29 +0530

    added middlewares

---
+++ Middlewares_and_cors/index.js
@@ -0,0 +1,33 @@
+if (arr[i] % 2 === 0)
+    even.push(arr[i])
+const express = require("express")
+const app = express()
+// total number of requesst 
+let requestcount = 0
+function requestIncreaser() {
+    requestcount = requestcount + 1
+
+    app.get("sum", function (req, res) {
+        requestIncreaser()
+        //main logic
+        const a = parseInt(req.query.a)
+        const b = parseInt(req.query.b)
+
+        res.json({
+            ans: a + b
+        })
+    })
+    app.get("sum", function (req, res) {
+        requestIncreaser()
+        //main logic
+        const a = parseInt(req.query.a)
+        const b = parseInt(req.query.b)
+
+        res.json({
+            ans: a + b
+        })
+    })
+}
+app.listen(3000)
+
+
