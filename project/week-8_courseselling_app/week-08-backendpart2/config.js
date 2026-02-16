8f35a7fc953a21a8a718b0c1557c39d5d1e840ae
Shilpa-Pal <shilpapal1721@gmail.com>
2026-02-16 15:44:53 +0530

    added .env file.

--- project/week-8_courseselling_app/week-08-backendpart2/config.js
+++ project/week-8_courseselling_app/week-08-backendpart2/config.js
@@ -1,8 +1,10 @@
 
-const JWT_USER_PASSORD = "shh234"
-const JWT_ADMIN_PASSWORD = "jkdd234"
+const JWT_USER_PASSORD = process.env.JWT_USER_PASSWORD; // Access the JWT secret for user authentication
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD; // Access the JWT secret for admin authentication
 
-module.exports ={
-    JWT_ADMIN_PASSWORD,
-    JWT_USER_PASSORD
+
+// Export the retrieved JWT passwords so they can be used in other modules
+module.exports = {
+    JWT_ADMIN_PASSWORD,// Exporting the admin JWT password
+    JWT_USER_PASSORD// Exporting the user JWT password
 }
